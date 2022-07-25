685acd326c6549f417cfc3a45f80e0fb514d6539
jkvargas <nm%vkjJ6@TXBtD@pPYycT>
2022-07-25 16:36:08 -0700

    need to add async-trait to implementation.
    setup function only prepares swarm and transport.
    so far sucess in encapsulating libp2p

--- src/peer_to_peer_service/mod.rs
+++ src/peer_to_peer_service/mod.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use libp2p::gossipsub::{
     Gossipsub, GossipsubConfigBuilder, MessageAuthenticity, MessageId, ValidationMode,
 };
@@ -14,55 +14,66 @@ pub trait PeerToPeerContract {
     fn listen(&mut self) -> Result<()>;
 }
 
-pub struct PeerToPeerService<'a> {
+pub struct PeerToPeerService {
     // we will need a local key in order to generate an identity
     local_key: Keypair,
     // peer id is the our identification in a p2p network
     peer_id: PeerId,
-    gossip: &'a Gossipsub,
-    swarm: Swarm<Gossipsub>,
+    swarm: Option<Swarm<Gossipsub>>,
 }
 
-impl<'a> PeerToPeerService<'a> {
-    pub fn new() -> Result<Self> {
-        // let local_key = Keypair::generate_ed25519();
-        // let peer_id = PeerId::from(&local_key);
-        //
-        // let gossip = Gossipsub::new(
-        //     MessageAuthenticity::Signed(local_key.clone()),
-        //     GossipsubConfigBuilder::default()
-        //         .heartbeat_interval(std::time::Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
-        //         .validation_mode(ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
-        //         .message_id_fn(|message| {
-        //             let mut s = DefaultHasher::new();
-        //             message.data.hash(&mut s);
-        //             MessageId::from(s.finish().to_string())
-        //         }) // content-address messages. No two messages of the
-        //         // same content will be propagated.
-        //         .build()?,
-        // )?;
-        //
-        // let gossip_ref = &gossip;
-        // // Set up an encrypted TCP Transport over the Mplex and Yamux protocols
-        // let transport = libp2p::development_transport(local_key).await?;
-        //
-        // // build a gossipsub network behaviour
-        // let swarm = Swarm::new(transport, gossip, peer_id.clone());
-        //
-        // Ok(PeerToPeerService {
-        //     local_key: local_key.clone(),
-        //     peer_id,
-        //     gossip: gossip_ref,
-        //     swarm,
-        // })
+impl PeerToPeerService {
+    pub fn new() -> Self {
+        let local_key = Keypair::generate_ed25519();
+        let peer_id = PeerId::from(&local_key.public());
 
-        todo!()
+        Self {
+            local_key,
+            peer_id,
+            swarm: None,
+        }
+    }
+
+    pub async fn setup(&mut self) -> Result<()> {
+        let local_key = Keypair::generate_ed25519();
+        let peer_id = PeerId::from(&local_key.public());
+
+        let config = GossipsubConfigBuilder::default()
+            .heartbeat_interval(std::time::Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
+            .validation_mode(ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
+            .message_id_fn(|message| {
+                let mut s = DefaultHasher::new();
+                message.data.hash(&mut s);
+                MessageId::from(s.finish().to_string())
+            }) // content-address messages. No two messages of the
+            // same content will be propagated.
+            .build().map_err(|err| anyhow!(err))?;
+
+        let gossip = Gossipsub::new(
+            MessageAuthenticity::Signed(local_key.clone()),
+            config,
+        ).map_err(|err| anyhow!(err))?;
+
+        // Set up an encrypted TCP Transport over the Mplex and Yamux protocols
+        let transport = libp2p::development_transport(local_key).await?;
+
+        // build a gossipsub network behaviour
+        let swarm = Swarm::new(transport, gossip, peer_id.clone());
+
+        self.swarm = Some(swarm);
+
+        Ok(())
     }
 }
 
-impl<'a> PeerToPeerContract for PeerToPeerService<'a> {
+impl<'a> PeerToPeerContract for PeerToPeerService {
     fn listen(&mut self) -> Result<()> {
-        todo!()
+        if let Some(swm) = &mut self.swarm {
+            swm.listen_on("/ip4/0.0.0.0/tcp/0".parse().unwrap())?;
+            return Ok(());
+        }
+
+        Err(anyhow!("You need to setup the service first"))
     }
 }
 
@@ -70,9 +81,10 @@ impl<'a> PeerToPeerContract for PeerToPeerService<'a> {
 mod when_using_peer_to_peer_service {
     use crate::peer_to_peer_service::{PeerToPeerContract, PeerToPeerService};
 
-    #[test]
-    fn listen_must_work() {
-        let mut service = PeerToPeerService::new().unwrap();
+    #[tokio::test]
+    async fn listen_must_work() {
+        let mut service = PeerToPeerService::new();
+        service.setup().await.unwrap();
         service.listen().unwrap();
     }
 }
